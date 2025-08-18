20ddf2c5a639a02f894b8af78bceb510de492354
Abhishek Singh <abhishekkhhsingh@gmail.com>
2025-08-18 16:58:38 +0530

    Implement CRUD functionality for Todo app: add, update, and delete features with UI enhancements

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-# Import Flask and render_template from the flask module
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from datetime import datetime
 
-# Create an instance of the Flask class (the web app)
 app = Flask(__name__)
-
-# Initialize SQLAlchemy 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Defining the schema of database 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -21,30 +16,45 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-# Create the database if it doesn't exist
-with app.app_context():
-    db.create_all()
-
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    if request.method == 'POST':
+def hello_world():
+    if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-
-        # Create a new Todo entry
         todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-
-    # Always fetch all todos for rendering
-    allTodo = Todo.query.all()
-    return render_template('index.html', allTodo=allTodo) 
+        
+    allTodo = Todo.query.all() 
+    return render_template('index.html', allTodo=allTodo)
 
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return render_template("index.html", allTodo=allTodo)
+    return 'this is products page'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
